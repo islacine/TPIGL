c8f66075ea3e5203aeda5e63d4b5bd8217bee883
YacineMestoui <YacineMestoui@DESKTOP-3M4EK18>
2017-10-18 19:33:18 +0100

    La calsse test non complet

---
+++ src/TestVectorHelper.java
@@ -0,0 +1,32 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author YacineMestoui
+ */
+public class TestVectorHelper {
+    @Test
+    public void sommeVec() throws LengthException {
+        
+        int tableauEntier[] = {0,1,2,3,4,5,6,7,8,9};
+        //int tableauEntier[] = {0,1,2,3,4,5,6,7,8,9};
+    }
+
+    @Test
+    public void multiplicationScal() {
+        
+        //assertArrayEquals(, );
+    }
+
+}
